612fb20888e07a7a7c2ac43a9c5d18bce3a23498
Ethan Keats <crazyeman83@gmail.com>
2025-09-05 21:37:13 -0400

    First Commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+
+
+
+	description = "A very basic flake";
+
+
+
+	inputs = {
+
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+	};
+
+
+
+	outputs = { self, nixpkgs }: {
+	
+		nixosConfigurations.ethan-nixos = nixpkgs.lib.nixosSystem {
+
+			modules = [ ./configuration.nix ];
+
+		};
+
+	};
+
+	
+
+}
