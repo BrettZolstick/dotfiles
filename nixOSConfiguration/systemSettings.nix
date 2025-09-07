a4b4d4d3ea011c9104017f34ec99d8691dc160c4
Ethan Keats <crazyeman83@gmail.com>
2025-09-07 14:41:46 -0400

    cleaned up systemSettings.nix

--- nixOSConfiguration/systemSettings.nix
+++ nixOSConfiguration/systemSettings.nix
@@ -5,45 +5,65 @@
 		/etc/nixos/hardware-configuration.nix
 	];
 
-	# Bootloader.
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
 
-	# Use latest kernel.
-	boot.kernelPackages = pkgs.linuxPackages_latest;
 
-	# Hostname
-	networking.hostName = "ethan-nixos";
-	
-	# Enable networking
-	networking.networkmanager.enable = true;
-
-	# Experimental features
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-	# Set your time zone.
-	time.timeZone = "America/New_York";
-
-	# Select internationalisation properties.
-	i18n.defaultLocale = "en_US.UTF-8";
-
-	i18n.extraLocaleSettings = {
-		LC_ADDRESS = "en_US.UTF-8";
-		LC_IDENTIFICATION = "en_US.UTF-8";
-		LC_MEASUREMENT = "en_US.UTF-8";
-		LC_MONETARY = "en_US.UTF-8";
-		LC_NAME = "en_US.UTF-8";
-		LC_NUMERIC = "en_US.UTF-8";
-		LC_PAPER = "en_US.UTF-8";
-		LC_TELEPHONE = "en_US.UTF-8";
-		LC_TIME = "en_US.UTF-8";
+	# Boot Options	
+	boot = {
+		loader = {
+			systemd-boot.enable = true
+			efi.canTouchEfiVariables = true;
+		};
+		
+		kernalPackages = pkgs.linuxPackages_latest;
 	};
 
+
+
+	# Networking Options
+	networking = {
+		hostname = "ethan-nixos";
+		networkmanager.enable = true;
+	};
+
+
+
+	# Experimental Features
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+
+
+	# Time Zone
+	time.timeZone = "America/New_York";
+
+
+
+	# Select internationalisation properties.
+	i18n = {
+		defaultLocale = "en_US.UTF-8";
+
+		extraLocaleSettings = {
+			LC_ADDRESS = "en_US.UTF-8";
+			LC_IDENTIFICATION = "en_US.UTF-8";
+			LC_MEASUREMENT = "en_US.UTF-8";
+			LC_MONETARY = "en_US.UTF-8";
+			LC_NAME = "en_US.UTF-8";
+			LC_NUMERIC = "en_US.UTF-8";
+			LC_PAPER = "en_US.UTF-8";
+			LC_TELEPHONE = "en_US.UTF-8";
+			LC_TIME = "en_US.UTF-8";
+		};
+			
+	};
+
+
+	
 	# Configure keymap in X11
 	services.xserver.xkb = {
 		layout = "us";
 		variant = "";
 	};
+
+
 
 	# Define a user account.
 	users.users.ethan = {
@@ -53,8 +73,12 @@
 		packages = with pkgs; [];
 	};
 
+
+
 	# Allow unfree packages
 	nixpkgs.config.allowUnfree = true;
+
+
 
 	# This value determines the NixOS release from which the default
 	# settings for stateful data, like file locations and database versions
